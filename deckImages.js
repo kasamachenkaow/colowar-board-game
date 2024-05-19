75df339ef8e8395aea65f5ecafb30523a32be0fc
Admin SSG <admin@secure-smart-guard.com>
2024-05-19 14:32:59 +0200

    feat: draw image cards from decks

---
+++ deckImages.js
@@ -0,0 +1,20 @@
+const spellDeck = [
+    'spell_basic_02_lv1.png',
+    'spell_basic_03_lv1.png',
+    'spell_basic_04_lv1.png',
+    'spell_basic_lv1.png',
+];
+
+const equipmentDeck = [];
+
+const skillDeck = [
+    'skill_basic.png',
+    'skill_basic_02.png',
+    'skill_basic_03.png',
+];
+
+const deckImages = {
+    spell: spellDeck,
+    equipment: equipmentDeck,
+    skill: skillDeck
+};
